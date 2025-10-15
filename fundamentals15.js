f432c26df150ef668fdb4406c5454bf5fb7bb634
masuddev2 <tuhin.semicolonit@gmail.com>
2025-10-15 11:08:42 +0600

    javascript fundameltal

---
+++ fundamentals15.js
@@ -0,0 +1,70 @@
+// Data types categories
+
+// 1. primitive
+// 2. Non primitive
+
+// * primitive
+
+// 1. String
+// 2  Number
+// 3. Boolean
+// 4. null
+// 5. undefined
+// 6. BigInt
+// 7. Symbol
+
+// * non primitive
+
+// 1 . Object
+// 2.  Array
+// 3.  Function
+
+// let a = "Hello 2152";
+// let b = 10; //intizer
+// let ba = 10.1; //float
+// let c = true;
+
+// const f = null;
+
+// let d = undefined;
+
+// let dd = 12354687598n;
+
+// let df = Symbol("id");
+
+// console.log("string111", typeof a);
+// console.log("stri", typeof b);
+// console.log("string111", typeof ba);
+// console.log("string111", typeof c);
+// console.log("string111", typeof f);
+// console.log("string111", typeof d);
+// console.log("string111", typeof dd);
+// console.log("string111", typeof df);
+
+// * Non primitive
+
+// let a = { age: 10, name: "my name", addr: "address" };
+// let b = ["10", "10", "11111", "6666"];
+// let c = function name() {};
+
+// console.log("obj", typeof a);
+// console.log("arr", typeof b);
+// console.log("func", typeof c);
+
+// window
+
+// console.log(typeof window);
+
+// let x = "5";
+
+// const y = x == 5;
+// let x = "5";
+
+// const y = x === 5;
+// let x = "5";
+
+// const y = x != 5;
+// let x = "5";
+
+// const y = x !== 5;
+// console.log(y);
---
+++ index.js
@@ -0,0 +1,32 @@
+// var
+// let
+// const
+
+// var ritu = "good girl";
+
+// ??
+
+// var ritu = "bad girl";
+
+// let ritu = "girl";
+
+// ritu = 30;
+
+// const tamimAge = 25;
+// tamimAge = "good";
+
+// console.log(tamimAge);
+
+// var x = 10;
+// var x = 20;
+
+// console.log(x);
+
+// let y = 10;
+// y = 20;
+
+// console.log(y);
+
+// const x = 10;
+
+// console.log(z);
