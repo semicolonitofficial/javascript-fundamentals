f71cb6427d9e6179680d19e9499e426b8a3e1ca8
masuddev2 <tuhin.semicolonit@gmail.com>
2025-10-20 10:59:45 +0600

    dom

---
+++ dom.js
@@ -0,0 +1,14 @@
+// console.log("conect");
+
+// console.log(document);
+
+const heading = document.getElementById("heading");
+
+// let text = heading.innerHTML;
+
+function clickMe() {
+  heading.innerHTML = "This is second heding";
+  heading.style.color = "red";
+}
+
+console.log("heading", text);
